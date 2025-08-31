09093ee0f76a984eb8389d78cba7472df770a49b
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2025-08-31 19:48:32 +0300

    Add loading

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { LogoIcon } from "@/components/icons/logo";
+import { Loader2 } from "lucide-react";
+
+export default function Loading() {
+    return (
+        <div className="flex flex-col w-full gap-4 h-dvh items-center justify-center">
+            <LogoIcon className="h-20 w-20"/>
+            <Loader2 className="h-20 w-20 animate-spin text-blue-300" />
+        </div>
+    );
+}
